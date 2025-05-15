a8281bdf2f07c011672544304f04b2d9097574fc
Dheeraj Rawandhe <Dheerajrawandhe57@gmail.com>
2025-05-16 01:51:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+  origin: "https://e-learning-n74g.onrender.com",
     credentials:true
 }));
 
