4d36543833217d04d7d0f5b15d4939a2675d82ac
Dheeraj Rawandhe <Dheerajrawandhe57@gmail.com>
2025-05-23 15:08:13 +0530

    Update authApi.js

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -2,7 +2,9 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
 // const USER_API = "http://localhost:8080/api/v1/user/"   // Localhost -> my laptop
-const USER_API = import.meta.env.VITE_API_URL;        // public -> all deployee
+// const USER_API = import.meta.env.VITE_API_URL;        // public -> all deployee
+const USER_API =`https://e-learningbackend-zow1.onrender.com/`;        // public -> all deployee
+
 
 
 export const authApi = createApi({reducerPath:"authApi",
