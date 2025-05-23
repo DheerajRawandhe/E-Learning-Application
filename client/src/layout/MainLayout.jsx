0e1d08e2991ab68bd96332e19ed109454f9bc22a
Demo Project <demoProject@gmail.com>
2025-05-23 21:59:24 +0530

    first commit

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -5,9 +5,9 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
   <>
-     <div className="flex bg-[rgb(231,228,235)]  flex-col min-h-screen">
+     {/* <div className="flex bg-[rgb(231,228,235)]  flex-col min-h-screen"> */}
     {/* // <div className="flex bg-[#fbfbfb]  flex-col min-h-screen"> */}
-    {/* <div className="flex flex-col min-h-screen">   */}
+    <div className="flex flex-col min-h-screen">  
       <Navbar />
       <div className="flex-1 mt-16">
         <Outlet />
